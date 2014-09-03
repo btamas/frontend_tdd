047cad246f95febedec3f791e7a1a9da536fb92b
btamas <tamas@besenyei.net>
2014-09-03 14:14:20 +0200

    jsdoc integration

--- libs/romanCalculator.js
+++ libs/romanCalculator.js
@@ -1,5 +1,15 @@
+/**
+ * RomanCalculator
+ *
+ * @constructor
+ */
 var RomanCalculator = function() {};
 
+/**
+ * Check is this a roman calculator
+ *
+ * @returns {boolean}
+ */
 RomanCalculator.prototype.isRomanCalculator = function() {
 	return true;
 };
