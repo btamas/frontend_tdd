58ee2d27f5c000554b72c1a1dec799e6916ecb8a
btamas <tamas@besenyei.net>
2014-09-03 21:54:59 +0200

    comment + jsdoc

--- libs/romanCalculator.js
+++ libs/romanCalculator.js
@@ -1,15 +1,38 @@
+/**
+ * RomanCalculator
+ *
+ * @constructor
+ */
 var RomanCalculator = function() {
+	/**
+	 * Contains roman decimal places
+	 * @type {string[]}
+	 * @private
+	 */
 	this._romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+	/**
+	 * Contains arabic decimal places
+	 * @type {number[]}
+	 * @private
+	 */
 	this._arabics = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 };
 
-RomanCalculator.prototype._convertToArabic = function(romanNumber) {
+/**
+ * Convert roman number to arabic
+ *
+ * @param {string} roman - Roman number
+ * @returns {number} Arabic number
+ * @private
+ */
+RomanCalculator.prototype._convertToArabic = function(roman) {
 	var	arabic = 0;
 
 	for (var i = 0; i < this._romans.length; i++) {
-		if (romanNumber.indexOf(this._romans[i]) === 0) {
+		if (roman.indexOf(this._romans[i]) === 0) {
 			arabic += this._arabics[i];
-			romanNumber = romanNumber.substring(this._romans[i].length);
+			roman = roman.substring(this._romans[i].length);
 			i--;
 		}
 	}
@@ -17,6 +40,12 @@ RomanCalculator.prototype._convertToArabic = function(romanNumber) {
 	return arabic;
 };
 
+/**
+ * Convert arabic numbers to roman
+ * @param {number} arabic - Arabic number
+ * @returns {string} Roman number
+ * @private
+ */
 RomanCalculator.prototype._convertToRoman = function(arabic) {
 	var roman = '';
 
@@ -31,6 +60,13 @@ RomanCalculator.prototype._convertToRoman = function(arabic) {
 	return roman;
 };
 
+/**
+ * Summarize two roman number
+ * @param {string} roman1 - First roman number
+ * @param {string} roman2 - Second roman number
+ * @throws Too big number
+ * @returns {string} Result roman number
+ */
 RomanCalculator.prototype.summarize = function(roman1, roman2) {
 	var arabic1 = this._convertToArabic(roman1),
 		arabic2 = this._convertToArabic(roman2),
