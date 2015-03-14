615fe66fca24ca0b37d90209d13061c5b888272f
btamas <tamas@besenyei.net>
2015-03-14 18:46:28 +0100

    Task2

--- libs/parser.ts
+++ libs/parser.ts
@@ -4,11 +4,37 @@ import Exception = require('./exception');
 import _ = require('underscore');
 
 export class Parser {
-    public parse(text:String):Array<String> {
-        if (_.isString(text)) {
-            return text.split(',');
+    private static COLUMN_SEPARATOR = ',';
+    private static LINE_SEPARATOR = '\n';
+
+    private getColumnsFromLine(text: String): Array<String> {
+        return text.split(Parser.COLUMN_SEPARATOR);
+    }
+
+    private getLines(text: String): Array<String> {
+        return text.split(Parser.LINE_SEPARATOR);
+    }
+
+    private getColumnsFromLines(text: String): String[][] {
+        var lines = this.getLines(text);
+        var columns = _.map(lines, (line) => {
+            return this.getColumnsFromLine(line);
+        });
+
+        return columns;
+    }
+
+    public parse(text:String):Array<any> {
+        if (!_.isString(text)) {
+            throw new Exception.InvalidInputException();
         }
 
-        throw new Exception.InvalidInputException();
+        var columns = this.getColumnsFromLines(text);
+
+        if (columns.length === 1) {
+            return columns[0];
+        }
+
+        return columns;
     }
 }
--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -7,7 +7,7 @@ import Exception = require('../libs/exception');
 
 class ParserSuite extends Test.Suite {
     public name = 'Parser';
-    private parser: Parser.Parser;
+    private parser:Parser.Parser;
 
     public setup() {
         this.parser = new Parser.Parser();
@@ -16,7 +16,8 @@ class ParserSuite extends Test.Suite {
 
 class OneLineTest extends Test.Test {
     public name = 'one-line string input';
-    private parser: Parser.Parser;
+    private parser:Parser.Parser;
+
     public dataProvider() {
         return [
             ['', ['']],
@@ -28,6 +29,7 @@ class OneLineTest extends Test.Test {
             ['Mark,Anthony,marka@lib.de', ['Mark', 'Anthony', 'marka@lib.de']]
         ];
     }
+
     public test(input, output) {
         assert.deepEqual(this.parser.parse(input), output);
     }
@@ -35,7 +37,8 @@ class OneLineTest extends Test.Test {
 
 class OneLineIsNotString extends Test.Test {
     public name = 'one-line input is not string';
-    private parser: Parser.Parser;
+    private parser:Parser.Parser;
+
     public dataProvider() {
         return [
             null,
@@ -53,8 +56,47 @@ class OneLineIsNotString extends Test.Test {
     }
 }
 
+class MultiLineInput extends Test.Test {
+    public name = 'multi-line string input';
+    private parser:Parser.Parser;
+
+    public dataProvider() {
+        return [
+            [
+                '\n',
+                [[''], ['']]
+            ],
+            [
+                'a\na',
+                [['a'], ['a']]
+            ],
+            [
+                'app,le\n42\ncat',
+                [['app', 'le'], ['42'], ['cat']]
+            ],
+            [
+                '1\n , \n2',
+                [['1'], [' ', ' '], ['2']]
+            ],
+            [
+                '211,22,35\n10,20,33',
+                [['211', '22', '35'], ['10', '20', '33']]
+            ],
+            [
+                'luxembourg,kennedy,44\nbudapest,expo ter,5-7\ngyors,fo utca,9',
+                [['luxembourg', 'kennedy', '44'], ['budapest', 'expo ter', '5-7'], ['gyors', 'fo utca', '9']]
+            ]
+        ];
+    }
+
+    public test(input:String, output:Array<String>) {
+        assert.deepEqual(this.parser.parse(input), output);
+    }
+}
+
 var parserSuite = new ParserSuite([
     new OneLineTest(),
-    new OneLineIsNotString()
+    new OneLineIsNotString(),
+    new MultiLineInput()
 ]);
 parserSuite.run();
