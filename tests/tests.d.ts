c0129965605b01585fe5ab041199514da6c4c806
btamas <tamas@besenyei.net>
2015-03-18 21:45:03 +0100

    remove old stuffs

--- tests/tests.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../types/node/node.d.ts" />
-/// <reference path="../types/mocha/mocha.d.ts" />
-/// <reference path="../types/chai/chai.d.ts" />
-/// <reference path="../types/underscore/underscore.d.ts" />
